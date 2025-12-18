28b69adf98ffd072b4a531b7481b4f57c8821c54
PULAK-SN <pulak.official22@gmail.com>
2025-12-18 12:28:19 +0530

    deployment

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.send(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+app.listen(PORT, () => {
+  console.log("Server is running at port: " + PORT);
+});
